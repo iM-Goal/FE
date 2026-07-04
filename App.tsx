7bd71433e308e726704d2b87ca46dca8cee45805
나오니 <beaknawon@LG-gram.local>
2026-07-04 17:58:40 +0900

    목표 말하기 기능 화면

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LoginScreen from './src/screens/LoginScreen';
-import SignupScreen from './src/screens/SignupScreen';
+//import LoginScreen from './src/screens/LoginScreen';
+//import SignupScreen from './src/screens/SignupScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import AddGoalScreen from './src/screens/AddGoalScreen';
+import VoiceRecordScreen from './src/screens/VoiceRecordScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,16 +14,17 @@ export default function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator
-            initialRouteName="Login" // 첫 시작점은 로그인 화면!
+            /*initialRouteName="Login" // 첫 시작점은 로그인 화면!
             screenOptions={{
               headerShown: false, // 피그마 커스텀 헤더를 쓸 것이므로 네이티브 상단 바는 숨김
               cardStyle: { backgroundColor: '#F8FAFC' }
-            }}
+            }}*/
         >
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
+          {/*<Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />*/}
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="AddGoal" component={AddGoalScreen} />
+            <Stack.Screen name="VoiceRecord" component= {VoiceRecordScreen} />
         </Stack.Navigator>
       </NavigationContainer>
   );
